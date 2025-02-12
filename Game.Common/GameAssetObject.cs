69943f440ad355853199614dff401a60ae05dee6
linfuqing <linfuqing@126.com>
2025-02-12 15:07:28 +0800

    version 2.11.9

---
+++ Game.Common/GameAssetObject.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class GameAssetObject : ZG.AssetObjectBase
+{
+    private Coroutine __coroutine;
+    
+    [SerializeField]
+    internal Space _space;
+    [SerializeField]
+    internal float _time;
+    [SerializeField, UnityEngine.Serialization.FormerlySerializedAs("label")]
+    internal string _fileName;
+
+    public override Space space => _space;
+
+    public override float time => _time;
+
+    public override string fileName => _fileName;
+
+    public override ZG.AssetManager assetManager => GameAssetManager.instance.dataManager;
+
+    protected new void OnEnable()
+    {
+        if (__coroutine != null)
+        {
+            var assetManager = GameAssetManager.instance;
+            if(assetManager != null)
+                assetManager.StopCoroutine(__coroutine);
+
+            __coroutine = null;
+            
+            base.OnDisable();
+        }
+        
+        base.OnEnable();
+    }
+
+    protected new void OnDisable()
+    {
+        var assetManager = GameAssetManager.instance;
+        if(assetManager != null)
+            __coroutine = assetManager.StartCoroutine(__Disable());
+    }
+
+    private System.Collections.IEnumerator __Disable()
+    {
+        yield return null;
+
+        base.OnDisable();
+
+        __coroutine = null;
+    }
+}
